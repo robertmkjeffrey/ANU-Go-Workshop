a907efb4599f3f8a6841489040976bbf7f40b630
Robert Jeffrey <robert.jeffrey@anu.edu.au>
2021-10-15 13:57:35 +1100

    Fixed bad merge.

--- waiting/waiting.go
+++ waiting/waiting.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-<<<<<<< HEAD
 	"log"
-=======
-	"fmt"
 	"sync"
->>>>>>> Added solution for waiting
 	"time"
 )
 
@@ -20,25 +16,17 @@ func main() {
 	// TODO: Use a WaitGroup to ensure the second message prints.
 	// Make sure it's still concurrent!
 
-<<<<<<< HEAD
-	delay_print("Sequentially, the main function waits for us to finish...")
-	go delay_print("Did you see me?")
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	delay_print("Sequentially, the main function waits for us to finish...", &wg)
+
+	wg.Add(1)
+	go delay_print("Did you see me?", &wg)
+
 	log.Println("...but it might not concurrently.")
 	log.Println("Unlike tasks in Ada, goroutines halt when their parent goroutine returns.")
 	log.Println("How can you ensure the concurrent print gets to finish?")
-=======
-	var print_wg sync.WaitGroup
 
-	print_wg.Add(1)
-	delay_print("Sequentially, the main function waits for us to finish...", &print_wg)
-
-	print_wg.Add(1)
-	go delay_print("Did you see me?", &print_wg)
-
-	fmt.Println("...but it might not concurrently.")
-	fmt.Println("Unlike tasks in Ada, goroutines halt when their parent goroutine returns.")
-	fmt.Println("How can you ensure the concurrent print gets to finish?")
-
-	print_wg.Wait()
->>>>>>> Added solution for waiting
+	wg.Wait()
 }
